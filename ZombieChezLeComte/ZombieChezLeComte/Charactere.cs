4e8918490153e2df809933965376bcca966f3a62
Saml74 <samuel.parent@etu.univ-smb.fr>
2023-01-03 16:42:14 +0100

    Animation dans la classe charactère

--- ZombieChezLeComte/ZombieChezLeComte/Charactere.cs
+++ ZombieChezLeComte/ZombieChezLeComte/Charactere.cs
@@ -3,11 +3,14 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using MonoGame.Extended.Screens;
 using MonoGame.Extended.Screens.Transitions;
+using MonoGame.Extended.Serialization;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MonoGame.Extended.Sprites;
+using MonoGame.Extended.Content;
 
 namespace ZombieChezLeComte
 {
@@ -16,6 +19,9 @@ namespace ZombieChezLeComte
         private Texture2D _texture;
         private Vector2 _position;
         private int _vitesse;
+        private AnimatedSprite _perso;
+        private SpriteSheet _spriteSheet;
+        private String _currentAnimation;
 
         public Texture2D Texture
         {
@@ -53,23 +59,60 @@ namespace ZombieChezLeComte
                 this._vitesse = value;
             }
         }
+        public AnimatedSprite Perso
+        {
+            get
+            {
+                return this._perso;
+            }
 
+            set
+            {
+                this._perso = value;
+            }
+        }
+        public SpriteSheet SpriteSheet
+        {
+            get
+            {
+                return this._spriteSheet;
+            }
+
+            set
+            {
+                this._spriteSheet = value;
+            }
+        }
+        public string CurrentAnimation
+        {
+            get
+            {
+                return this._currentAnimation;
+            }
+
+            set
+            {
+                this._currentAnimation = value;
+            }
+        }
 
         public void Initialize(Texture2D _texture2D, Vector2 _position, int _vitesse)
         {
             this.Position = _position;
             this.Texture = _texture2D;
             this.Vitesse = _vitesse;
+            this.CurrentAnimation="idle";
         }
 
-        public void LoadContent()
+        public void LoadContent(SpriteSheet _spriteSheet)
         {
-            
+            this.SpriteSheet = _spriteSheet;
+            this.Perso = new AnimatedSprite(_spriteSheet);
         }
 
         public void Update(GameTime gameTime)
         {
-
+            this.Perso.Play(this.CurrentAnimation);
         }
 
         public void Draw(SpriteBatch _spriteBatch)
@@ -80,6 +123,26 @@ namespace ZombieChezLeComte
         }
         public void Movement(Vector2 _vector2, float _delattime)
         {
+            if(_vector2.X == 0 && _vector2.Y == 0)
+            {
+                this.CurrentAnimation = "idle";
+            }
+            if(_vector2.X > 0)
+            {
+                this.CurrentAnimation = "estWalk";
+            }
+            if (_vector2.X <0)
+            {
+                this.CurrentAnimation = "westWalk";
+            }
+            if (_vector2.Y > 0)
+            {
+                this.CurrentAnimation = "southWalk";
+            }
+            if (_vector2.Y < 0)
+            {
+                this.CurrentAnimation = "northWalk";
+            }
             this.Position += _vector2 * this.Vitesse * _delattime;
         }
     }

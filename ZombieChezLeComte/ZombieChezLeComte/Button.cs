3a68ef3ef5abf0d9a7705cc74d435786b9c9aab7
DylanMif <Dylan.Miftari@etu.univ-savoie.fr>
2023-01-03 14:32:45 +0100

    Classe Button avancé

---
+++ ZombieChezLeComte/ZombieChezLeComte/Button.cs
@@ -0,0 +1,312 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace ZombieChezLeComte
+{
+    public class Button
+    {
+        private string text;
+        private string writeText;
+        private Color bgColor;
+        private Vector2 position;
+        private uint width;
+        private uint height;
+        private SpriteFont font;
+        private uint paddindTop;
+        private uint paddingLeft;
+
+        private string hoverText;
+        private Color currentColor;
+        private Color clickBgColor;
+
+        private Texture2D rectTex;
+
+        private Color textColor;
+        private Color hoverTextColor;
+        private Color clickTextColor;
+        private Color currentTextColor;
+
+        public string Text
+        {
+            get
+            {
+                return text;
+            }
+
+            set
+            {
+                text = value;
+            }
+        }
+
+        public Color BgColor
+        {
+            get
+            {
+                return bgColor;
+            }
+
+            set
+            {
+                bgColor = value;
+            }
+        }
+
+        public Vector2 Position
+        {
+            get
+            {
+                return position;
+            }
+
+            set
+            {
+                position = value;
+            }
+        }
+
+        public uint Width
+        {
+            get
+            {
+                return width;
+            }
+
+            set
+            {
+                width = value;
+            }
+        }
+
+        public uint Height
+        {
+            get
+            {
+                return height;
+            }
+
+            set
+            {
+                height = value;
+            }
+        }
+
+        public string HoverText
+        {
+            get
+            {
+                return hoverText;
+            }
+
+            set
+            {
+                hoverText = value;
+            }
+        }
+
+        public Color CurrentColor
+        {
+            get
+            {
+                return currentColor;
+            }
+
+            set
+            {
+                currentColor = value;
+            }
+        }
+
+        public Color ClickBgColor
+        {
+            get
+            {
+                return clickBgColor;
+            }
+
+            set
+            {
+                clickBgColor = value;
+            }
+        }
+
+        public Rectangle ButtonRect
+        {
+            get
+            {
+                return new Rectangle((int)this.Position.X, (int)this.Position.Y, (int)this.Width, (int)this.Height);
+            }
+        }
+
+        private string WriteText
+        {
+            get
+            {
+                return writeText;
+            }
+
+            set
+            {
+                writeText = value;
+            }
+        }
+
+        public SpriteFont Font
+        {
+            get
+            {
+                return font;
+            }
+
+            set
+            {
+                font = value;
+            }
+        }
+
+        public uint PaddindTop
+        {
+            get
+            {
+                return paddindTop;
+            }
+
+            set
+            {
+                paddindTop = value;
+            }
+        }
+
+        public uint PaddingLeft
+        {
+            get
+            {
+                return paddingLeft;
+            }
+
+            set
+            {
+                paddingLeft = value;
+            }
+        }
+
+        public Texture2D RectTex
+        {
+            get
+            {
+                return rectTex;
+            }
+
+            set
+            {
+                rectTex = value;
+            }
+        }
+
+        public Color TextColor
+        {
+            get
+            {
+                return textColor;
+            }
+
+            set
+            {
+                textColor = value;
+            }
+        }
+
+        public Color HoverTextColor
+        {
+            get
+            {
+                return hoverTextColor;
+            }
+
+            set
+            {
+                hoverTextColor = value;
+            }
+        }
+
+        public Color ClickTextColor
+        {
+            get
+            {
+                return clickTextColor;
+            }
+
+            set
+            {
+                clickTextColor = value;
+            }
+        }
+
+        public Color CurrentTextColor
+        {
+            get
+            {
+                return currentTextColor;
+            }
+
+            set
+            {
+                currentTextColor = value;
+            }
+        }
+
+        public void Initialize(string _text, Color _bgColor, Vector2 _position, uint _width, uint _height, Color _clickBgColor, 
+            uint _paddingTop, uint _paddingLeft, Color _textColor, Color _hoverTextColor, Color _clickTextColor)
+        {
+            this.Text = _text;
+            this.BgColor = _bgColor;
+            this.Position = _position;
+            this.Width = _width;
+            this.Height = _height;
+            this.ClickBgColor = _clickBgColor;
+            this.PaddindTop = _paddingTop;
+            this.PaddingLeft = _paddingLeft;
+            this.TextColor = _textColor;
+            this.HoverTextColor = _hoverTextColor;
+            this.ClickTextColor = _clickTextColor;
+        }
+
+        public void LoadContent(SpriteFont _font, Texture2D _rectText)
+        {
+            this.Font = _font;
+            this.RectTex = _rectText;
+        }
+
+        public void Update(MouseState _ms)
+        {
+            if (this.ButtonRect.Intersects(new Rectangle(_ms.Position, new Point(1, 1))))
+            {
+                this.WriteText = this.HoverText;
+            } else
+            {
+                this.WriteText = this.Text;
+            }
+
+            if(_ms.LeftButton == ButtonState.Pressed && this.ButtonRect.Intersects(new Rectangle(_ms.Position, new Point(1, 1))))
+            {
+                this.CurrentColor = this.ClickBgColor;
+            } else
+            {
+                this.CurrentColor = this.BgColor;
+            }
+        }
+
+        public void Draw(SpriteBatch _sb)
+        {
+            _sb.Begin();
+            _sb.Draw(this.RectTex, this.Position, this.CurrentColor);
+            _sb.DrawString(this.Font, this.WriteText, this.Position + new Vector2(this.PaddingLeft, this.PaddindTop), this.CurrentTextColor);
+        }
+    }
+}

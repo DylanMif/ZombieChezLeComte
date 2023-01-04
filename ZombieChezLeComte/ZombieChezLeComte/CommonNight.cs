897f847c7ba3cc522d3e2c3d69c8c977bff9e84f
Saml74 <samuel.parent@etu.univ-smb.fr>
2023-01-04 08:28:59 +0100

    Debut de CommonNight et de la nuit1

--- ZombieChezLeComte/ZombieChezLeComte/CommonNight.cs
+++ ZombieChezLeComte/ZombieChezLeComte/CommonNight.cs
@@ -7,10 +7,59 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
+using MonoGame.Extended.Tiled;
+using MonoGame.Extended.Tiled.Renderers;
 
 namespace ZombieChezLeComte
 {
-    internal class CommonNight
+    public class CommonNight
     {
+        private TiledMap _tiledMap;
+        private TiledMapRenderer _tiledMapRenderer;
+
+
+        public TiledMap TiledMap
+        {
+            get
+            {
+                return this._tiledMap;
+            }
+
+            set
+            {
+                this._tiledMap = value;
+            }
+        }
+        public TiledMapRenderer TiledMapRenderer
+        {
+            get
+            {
+                return this._tiledMapRenderer;
+            }
+
+            set
+            {
+                this._tiledMapRenderer = value;
+            }
+        }
+
+
+        public void Initialize()
+        {
+
+        }
+        public void LoadContent(GraphicsDevice _graphicsDevice, TiledMap _tilMap)
+        {
+            this.TiledMap = _tilMap;
+            this.TiledMapRenderer = new TiledMapRenderer(_graphicsDevice, this.TiledMap);
+        }
+        public void Update(GameTime _gameTime)
+        {
+            this.TiledMapRenderer.Update(_gameTime);
+        }
+        public void Draw()
+        {
+            this.TiledMapRenderer?.Draw();
+        }
     }
 }
--- ZombieChezLeComte/ZombieChezLeComte/Night1.cs
+++ ZombieChezLeComte/ZombieChezLeComte/Night1.cs
@@ -9,6 +9,8 @@ using Microsoft.Xna.Framework.Input;
 using MonoGame.Extended.Screens;
 using MonoGame.Extended.Screens.Transitions;
 using Microsoft.Xna.Framework.Media;
+using MonoGame.Extended.Tiled;
+using MonoGame.Extended.Tiled.Renderers;
 
 namespace ZombieChezLeComte
 {
@@ -17,25 +19,30 @@ namespace ZombieChezLeComte
         
         private new Game1 Game => (Game1)base.Game;
         public Night1(Game1 game) : base(game) { }
+        private CommonNight _nuit1 = new CommonNight();
 
         public override void Initialize()
         {
+            _nuit1.Initialize();
+
             base.Initialize();
         }
         public override void LoadContent()
         {
+            _nuit1.LoadContent(Game.GraphicsDevice, Game.Content.Load<TiledMap>("map"));
+
             base.LoadContent();
         }
 
         public override void Update(GameTime gameTime)
         {
-           
+            _nuit1.Update(gameTime);
         }
 
         public override void Draw(GameTime gameTime)
         {
             Game.GraphicsDevice.Clear(Color.White);
-
+            _nuit1.Draw();
         }
     }
 }

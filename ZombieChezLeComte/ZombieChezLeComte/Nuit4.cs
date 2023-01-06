af725468ea90de3b8adbb7235eb3911c98142c8b
Saml74 <samuel.parent@etu.univ-smb.fr>
2023-01-06 11:49:50 +0100

    Amelioration nuit4

--- ZombieChezLeComte/ZombieChezLeComte/Nuit4.cs
+++ ZombieChezLeComte/ZombieChezLeComte/Nuit4.cs
@@ -27,6 +27,8 @@ namespace ZombieChezLeComte
 
         private TextInfo textInfo = new TextInfo();
 
+        private Zombie _zombie = new Zombie();
+
         private bool estRecuperer = false;
         public bool possedeArme = false;
         private bool uneTache = false;
@@ -38,15 +40,16 @@ namespace ZombieChezLeComte
         private InteractObject[] kitchenPapers = new InteractObject[6];
         private InteractObject[] litInteractions = new InteractObject[4];
         private InteractObject[] laverLivres = new InteractObject[3];
-        private InteractObject[] morceauxArmes = new InteractObject[3];
         private InteractObject couteuxFour = new InteractObject();
         private InteractObject recupViande = new InteractObject();
+        private InteractObject debutArme = new InteractObject();
 
 
         public override void Initialize()
         {
             _nuit4.Initialize(Game.Window, Game.GraphicsDevice);
             textInfo.Initialize("erer", Color.White, new Vector2(10, Constantes.WINDOW_HEIGHT - 150));
+            //_zombie.Initialiaze();
             for (int i = 0; i < litInteractions.Length; i++)
             {
                 litInteractions[i] = new InteractObject();
@@ -64,7 +67,7 @@ namespace ZombieChezLeComte
             kitchenPapers[1].Initialize(new Vector2(3925, 6572), 31, 38, "papier2", "Laver les livres de la biblitotheque");
             kitchenPapers[2].Initialize(new Vector2(3955, 6632), 42, 35, "papier3", "");
             kitchenPapers[3].Initialize(new Vector2(4021, 6572), 30, 27, "papier4", "Mettre de la viande sur les lits");
-            kitchenPapers[4].Initialize(new Vector2(4086, 6571), 36, 27, "papier5", "");
+            kitchenPapers[4].Initialize(new Vector2(4086, 6571), 36, 27, "papier5", "Tout intrus doit etre eradique");
             kitchenPapers[5].Initialize(new Vector2(4120, 6605), 36, 27, "papier6", "Recuperer la viande dans le stockage");
             for (int i = 0; i < laverLivres.Length; i++)
             {
@@ -73,13 +76,15 @@ namespace ZombieChezLeComte
             laverLivres[0].Initialize(new Vector2(4482, 6381), 50, 50, "armoire1", "Les livres sont foutu maintenant");
             laverLivres[1].Initialize(new Vector2(4281, 6384), 50, 50, "armoire2", "Pourquoi laver les livres?");
             laverLivres[2].Initialize(new Vector2(4184, 6384), 50, 50, "armoire3", "Decidemment, ca devient de plus en plus bizzare...");
-            recupViande.Initialize(new Vector2(3918, 5718), 67, 702, "lit4", "Toujours la meme viande ragoutante...");
+            recupViande.Initialize(new Vector2(3918, 5718), 67, 702, "viande", "Toujours la meme viande ragoutante...");
+            debutArme.Initialize(new Vector2(0, 0), 32, 26, "arme", "J'ai une epee. Vite l'eau benite de la buandrie !");
             base.Initialize();
         }
         public override void LoadContent()
         {
             _nuit4.LoadContent(Game.GraphicsDevice, Game.Content.Load<TiledMap>("map"),
                 Game.Content.Load<SpriteSheet>("joueur.sf", new JsonContentLoader()));
+            //_zombie.LoadContent(Game.Content.Load<SpriteSheet>("zombie.sf", new JsonContentLoader()));
             textInfo.LoadContent(Game.Content.Load<SpriteFont>("MeanFont"));
             textInfo.Text = "Qu'est ce qui s'est passe? Je me suis evanouis je crois...";
             textInfo.ActiveText(Constantes.TEMPS_TEXTE);
@@ -158,7 +163,31 @@ namespace ZombieChezLeComte
                         }
                     }
                 }
-                if(nombreMorceauxArme == morceauxArmes.Length)
+                if ((nombreLitFait == litInteractions.Length && nombreLivre == laverLivres.Length && couteuxFour.HasAlreadyInteractable == true && nombreMorceauxArme == 0) || Keyboard.GetState().IsKeyDown(Keys.NumPad7))
+                {
+                    debutArme.InteractRect = new Rectangle(5072, 6125, debutArme.InteractRect.Width, debutArme.InteractRect.Height);
+                }
+                if (debutArme.InteractWith(-_nuit4.Camera.Position))
+                {
+                    if(nombreMorceauxArme == 0)
+                    {
+                        Additions.InteractionObjet(debutArme, textInfo, "Mon epee est benite. Il faut que je purifie cette arme dans le feu !");
+                        debutArme.InteractRect = new Rectangle(4778, 6193, 20, 23);
+                    }
+                    if (nombreMorceauxArme == 1)
+                    {
+                        Additions.InteractionObjet(debutArme, textInfo, "Deus Vult ! Je vais t'envoyer en enfer vile creature !");
+                        debutArme.InteractRect = new Rectangle(4990, 6382, 40, 3);
+                    }
+                    if (nombreMorceauxArme == 2)
+                    {
+                        textInfo.Text = "Deus Vult ! Je vais t'envoyer en enfer vile creature !";
+                        textInfo.ActiveText(Constantes.TEMPS_TEXTE);
+                        debutArme.InteractRect = new Rectangle(0, 0, 20, 23);
+                    }
+                    nombreMorceauxArme += 1;
+                }
+                if(nombreMorceauxArme == 3)
                 {
                     possedeArme = true;
                 }
@@ -172,6 +201,7 @@ namespace ZombieChezLeComte
             Game.SpriteBatch.Begin();
             textInfo.Draw(Game.SpriteBatch);
             Game.SpriteBatch.End();
+            //_zombie.Draw(Game.SpriteBatch);
         }
 
 

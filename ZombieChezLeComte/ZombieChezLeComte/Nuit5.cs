45dc78259e170698bd6bdd54b12205fc5e27fedb
DylanMif <Dylan.Miftari@etu.univ-savoie.fr>
2023-01-06 11:48:48 +0100

    Avancement nuit5

--- ZombieChezLeComte/ZombieChezLeComte/Nuit5.cs
+++ ZombieChezLeComte/ZombieChezLeComte/Nuit5.cs
@@ -30,6 +30,9 @@ namespace ZombieChezLeComte
         private RunGhost runGhost = new RunGhost();
         private Zombie zombie = new Zombie();
 
+        private InteractObject[] kitchenPapers = new InteractObject[6];
+        private TextInfo textInfo = new TextInfo();
+
         public override void Initialize()
         {
             commonNight.Initialize(Game.Window, Game.GraphicsDevice);
@@ -37,6 +40,19 @@ namespace ZombieChezLeComte
             doorGhost.Initialize(new Vector2(0, 0), 0);
             runGhost.Initialize(new Vector2(10, 10), Constantes.RUNGHOST_SPEED);
             zombie.Initialiaze(new Vector2(40, -210), Constantes.ZOMBIE_SPEED);
+
+            for (int i = 0; i < kitchenPapers.Length; i++)
+            {
+                kitchenPapers[i] = new InteractObject();
+            }
+            kitchenPapers[0].Initialize(new Vector2(3843, 6591), 23, 16, "papier1", "Ne mourrez pas et n'allez en aucun cas a la cave");
+            kitchenPapers[1].Initialize(new Vector2(3925, 6572), 31, 38, "papier2", "Le sommeil est la cle");
+            kitchenPapers[2].Initialize(new Vector2(3955, 6632), 42, 35, "papier3", "Lire revele la cle");
+            kitchenPapers[3].Initialize(new Vector2(4021, 6572), 30, 27, "papier4", "Ranger pour mieux retrouver vos affaires comme une cle...");
+            kitchenPapers[4].Initialize(new Vector2(4086, 6571), 36, 27, "papier5", "Se renforcer est la cle");
+            kitchenPapers[5].Initialize(new Vector2(4120, 6605), 36, 27, "papier6", "L'eau est la cle de la vie");
+
+            textInfo.Initialize(" ", Color.White, new Vector2(10, Constantes.WINDOW_HEIGHT - 150));
 
             base.Initialize();
         }
@@ -50,6 +66,8 @@ namespace ZombieChezLeComte
             doorGhost.LoadContent(Game.Content.Load<SpriteSheet>("fantomePorte.sf", new JsonContentLoader()));
             runGhost.LoadContent(Game.Content.Load<SpriteSheet>("fantomeRun.sf", new JsonContentLoader()));
             zombie.LoadContent(Game.Content.Load<SpriteSheet>("zombie.sf", new JsonContentLoader()));
+
+            textInfo.LoadContent(Game.Content.Load<SpriteFont>("MeanFont"));
             base.LoadContent();
         }
 
@@ -60,6 +78,20 @@ namespace ZombieChezLeComte
             doorGhost.Update(gameTime, commonNight, Game);
             runGhost.Update(gameTime, commonNight, Game);
             zombie.Update(gameTime, commonNight);
+
+            if(Keyboard.GetState().IsKeyDown(Keys.Space))
+            {
+                foreach (InteractObject paperInteract in kitchenPapers)
+                {
+                    if (paperInteract.InteractWith(-commonNight.Camera.Position))
+                    {
+                        textInfo.Text = paperInteract.InteractText;
+                        textInfo.ActiveText(2);
+                    }
+                }
+            }
+
+            textInfo.Update(gameTime);
         }
 
         public override void Draw(GameTime gameTime)
@@ -69,6 +101,9 @@ namespace ZombieChezLeComte
             doorGhost.Draw(Game.SpriteBatch);
             runGhost.Draw(Game.SpriteBatch);
             zombie.Draw(Game.SpriteBatch);
+            Game.SpriteBatch.Begin();
+            textInfo.Draw(Game.SpriteBatch);
+            Game.SpriteBatch.End();
         }
     }
 }

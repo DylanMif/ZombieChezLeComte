f818087e5849371131d088b2699198b76260299c
DylanMif <Dylan.Miftari@etu.univ-savoie.fr>
2023-01-06 16:24:44 +0100

    t

--- ZombieChezLeComte/ZombieChezLeComte/ScreenCave.cs
+++ ZombieChezLeComte/ZombieChezLeComte/ScreenCave.cs
@@ -3,10 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using MonoGame.Extended.Sprites;
+using MonoGame.Extended.Content;
+using MonoGame.Extended.Tiled;
+using MonoGame.Extended.Tiled.Renderers;
+using MonoGame.Extended;
+using MonoGame.Extended.ViewportAdapters;
+using MonoGame.Extended.Screens;
+using MonoGame.Extended.Screens.Transitions;
+using MonoGame.Extended.Serialization;
 
 namespace ZombieChezLeComte
 {
-    internal class ScreenCave
+    public class ScreenCave : GameScreen
     {
+        private new Game1 Game => (Game1)base.Game;
+        public ScreenCave(Game1 game) : base(game) { }
+
+        private Charactere player = new Charactere();
+
+        public override void Initialize()
+        {
+            player.Initialize(new Vector2(360, 360), Constantes.VITESSE_JOUEUR);
+            base.Initialize();
+        }
+
+        public override void LoadContent()
+        {
+            player.LoadContent(Game.Content.Load<SpriteSheet>("joueur.sf", new JsonContentLoader()));
+            base.LoadContent();
+        }
     }
 }
